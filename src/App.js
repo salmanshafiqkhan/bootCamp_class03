46d90fbc20ce0a75b4e80599f142934a2a5d7845
Salman Shafiq Khan <salmanshafiqkhan85@gmail.com>
2020-10-04 01:55:05 +0300

    update App.js
    Change the name from Package to Deal

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ function App() {
   return (
     <div className = "App">
       
-      <h1>Package 01</h1>
+      <h1>Deal 01</h1>
       <Restaurant dishName = "Chicken Mandi" dessert = "Kanafa"/>
-      <h1>Package 02</h1>
+      <h1>Deal 02</h1>
       <Restaurant dishName = "Mix Grill" dessert = "Umm Ali"/>
-      <h1 className="heading">Package 03</h1>
+      <h1>Deal 03</h1>
       <Restaurant dishName = "Chicken Mayo Roll" dessert = "Cheese Cake"/>
     </div>
   )
